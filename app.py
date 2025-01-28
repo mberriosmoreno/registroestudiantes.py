1b7ae611cebd7a2ede58fe9dbdb8291e05d0f869
mberriosmoreno <94083004+mberriosmoreno@users.noreply.github.com>
2025-01-28 13:52:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -15,10 +15,3 @@ st.markdown("Bienvenido a la aplicación de gestión de estudiantes. Selecciona 
 # Menú lateral
 st.sidebar.title("Menú")
 st.sidebar.markdown("Selecciona la operación que deseas realizar:")
-
-# Enlaces a las páginas
-st.sidebar.page_link("app.py", label="🏠 Inicio")
-st.sidebar.page_link("pages/1_📝_Registrar_Estudiante.py", label="📝 Registrar Estudiante")
-st.sidebar.page_link("pages/2_📋_Ver_Estudiantes.py", label="📋 Ver Estudiantes")
-st.sidebar.page_link("pages/3_✏️_Editar_Estudiante.py", label="✏️ Editar Estudiante")
-st.sidebar.page_link("pages/4_❌_Eliminar_Estudiante.py", label="❌ Eliminar Estudiante")
